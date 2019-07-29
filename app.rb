b1230428eba88f626545f951fd3c0c7e9010cd48
Lexxicus <alexxxicus@gmail.com>
2019-07-29 13:58:24 +0500

    comments didnt work

--- app.rb
+++ app.rb
@@ -2,15 +2,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
-require 'SQLite3'
+require 'sqlite3'
 #  создаем бд
-set :database, {adapter: 'sqlite3', database: 'lepra.db'}
+set :database, {adapter: "sqlite3", database: "lepra.db"}
 
 class Post < ActiveRecord::Base
-
+  has_many :comments
 end
 
 class Comment < ActiveRecord::Base
+  belongs_to :post
+end
 
+get '/' do
+  erb :index
 end
 
--- db/migrate/20190729062805_create_post.rb
+++ db/migrate/20190729062805_create_post.rb
@@ -2,7 +2,7 @@ class CreatePost < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :name
-      t.text :newpost
+      t.text :post
       t.datetime :published_at
       t.timestamps
     end
